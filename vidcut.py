827ea366fc278173ee65e08873cd8f3f0f6f7610
ssayand <krolexonder@gmail.com>
2023-02-26 16:51:23 +0400

    v1.2

--- vidcut.py
+++ vidcut.py
@@ -1,21 +1,35 @@
-from pytube import YouTube 
+import pytube
+import requests
+from colorama import Fore, init
 from moviepy.editor import AudioFileClip, VideoFileClip
-print("© 2023 Aneko")
+
+init(autoreset=True)
+print("© 2023 anekobtw")
+CURRENT_VERSION = "v1.2"
+
+def check_for_updates(current_version):
+    api_url = 'https://api.github.com/repos/anekobtw/youtube-clip-maker/releases/latest'
+    latest_version = requests.get(api_url).json()['tag_name']
+    if latest_version != current_version:
+        print(f"{Fore.RED}\nYou are using an old version of the app. A new version is already available here:\
+                            \nhttps://github.com/anekobtw/youtube-clip-maker\
+                            \nLatest Version: {latest_version}\n")
 
 def download_video(link):
-    YouTube(link).streams.filter(only_audio=True).first().download(filename="audio.mp3")
-    YouTube(link).streams.filter(res="1080p").first().download(filename="video_without_audio.mp4")
+    youtube = pytube.YouTube(link)
+    youtube.streams.filter(only_audio=True).first().download(filename="audio.mp3")
+    youtube.streams.filter(res="1080p").first().download(filename="video_without_audio.mp4")
 
 def cut_video(start_time, end_time):
     video_cutted = VideoFileClip("video_without_audio.mp4").subclip(start_time, end_time)
     audio_cutted = AudioFileClip("audio.mp3").subclip(start_time, end_time)
-    final_video = video_cutted.set_audio(audio_cutted)
-    final_video.write_videofile("cutted_video.mp4", logger=None)
+    video_cutted.set_audio(audio_cutted).write_videofile("cutted_video.mp4", logger=None)
 
 if __name__ == '__main__':
-    if input("Download video/stream? (yes/no)   ") in ["yes", "y", "YES", "Yes"]:
+    check_for_updates(CURRENT_VERSION)
+    if input("Download video/stream? (yes/no) ").lower() in ["yes", "y"]:
         download_video(input("Enter video link: "))
-    cut_video(input("Enter clip start time: "), input("Enter clip end time: "))
+    cut_video(float(input("Enter clip start time: ")), float(input("Enter clip end time: ")))
 
     print("Done!")
     input("Press Enter to exit ")
